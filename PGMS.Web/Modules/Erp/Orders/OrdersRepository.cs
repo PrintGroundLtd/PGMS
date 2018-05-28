32b77ace10af442b77113bb6a381175ba0ddfc09
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2018-05-28 20:54:05 +0300

    Fixed notes in order details (in order)

--- PGMS.Web/Modules/Erp/Orders/OrdersRepository.cs
+++ PGMS.Web/Modules/Erp/Orders/OrdersRepository.cs
@@ -1,5 +1,7 @@
 ﻿
+using System.Collections.Generic;
 using System.Linq;
+using PGMS.Administration.Entities;
 using PGMS.Erp.Entities;
 
 namespace PGMS.Erp.Repositories
@@ -42,7 +44,58 @@ namespace PGMS.Erp.Repositories
 
         private class MySaveHandler : SaveRequestHandler<MyRow> { }
         private class MyDeleteHandler : DeleteRequestHandler<MyRow> { }
-        private class MyRetrieveHandler : RetrieveRequestHandler<MyRow> { }
+
+        private class MyRetrieveHandler : RetrieveRequestHandler<MyRow>
+        {
+            protected override void OnReturn()
+            {
+                base.OnReturn();
+               
+                if (Response.Entity.DetailList.Any())
+                {
+                    Response.Entity.DetailList.ForEach(od =>
+                    {
+                        
+                        od.NoteList = new NoteRepository().List(Connection, new ListRequest
+                        {
+                            ColumnSelection = ColumnSelection.List,
+                            Criteria = 
+                                (new Criteria(NoteRow.Fields.EntityType.Name) == "[dbo].[OrderDetails]")
+                            & (new Criteria(NoteRow.Fields.EntityId.Name) == od.OrderDetailId.ToString())
+                        }).Entities;
+
+                        #region BUG With EMPTY NOTE list in master detail dialog 
+                        if (od.NoteList.Any())
+                        {
+
+                            // users might be in another database, in another db server, so we can't simply use a join here
+                            var userIdList = od.NoteList.Where(x => x.InsertUserId != null)
+                                .Select(x => x.InsertUserId.Value).Distinct();
+                            if (userIdList.Any())
+                            {
+                                var u = UserRow.Fields;
+                                IDictionary<int, string> userDisplayNames;
+                                using (var connection = SqlConnections.NewFor<UserRow>())
+                                    userDisplayNames = connection.Query(new SqlQuery()
+                                            .From(u)
+                                            .Select(u.UserId)
+                                            .Select(u.DisplayName)
+                                            .Where(u.UserId.In(userIdList)))
+                                        .ToDictionary(x => (int) (x.UserId ?? x.USERID), x => (string) x.DisplayName);
+
+                                string s;
+                                foreach (var x in od.NoteList)
+                                    if (x.InsertUserId != null &&
+                                        userDisplayNames.TryGetValue(x.InsertUserId.Value, out s))
+                                        x.InsertUserDisplayName = s;
+                            }
+                        }
+                        #endregion
+                    });
+                }
+
+            }
+        }
         private class MyListHandler : ListRequestHandler<MyRow, OrderListRequest>
         {
             protected override void OnReturn()
@@ -64,6 +117,7 @@ namespace PGMS.Erp.Repositories
                     responseEntity.Total = Decimal.Zero;
                     if (orderDetails.Any())
                         responseEntity.Total = orderDetails.Select(od => od.LineTotal).Aggregate((a, b) => a + b) ?? Decimal.Zero;
+
                 }
                 
             }
